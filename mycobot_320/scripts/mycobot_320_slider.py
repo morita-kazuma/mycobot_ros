1eab9bb7765a72b1425c2cc07d6f89292c853626
morita-kazuma <morita-kazuma@g.ecc.u-tokyo.ac.jp>
2022-07-27 12:39:44 +0900

    add comment

--- mycobot_320/scripts/mycobot_320_slider.py
+++ mycobot_320/scripts/mycobot_320_slider.py
@@ -25,7 +25,7 @@ def callback(data):
     for index, value in enumerate(data.position):
         data_list.append(value)
 
-    mc.send_radians(data_list, 80)
+    mc.send_radians(data_list, 80)#mycobotに角度を送る（pymycobotで設定した関数）
     # time.sleep(0.5)
 
 
@@ -33,8 +33,8 @@ def listener():
     global mc
     rospy.init_node("control_slider", anonymous=True)
 
-    rospy.Subscriber("joint_states", JointState, callback)
-    port = rospy.get_param("~port", "/dev/ttyUSB0")
+    rospy.Subscriber("joint_states", JointState, callback)#JointStateをsubscribe
+    port = rospy.get_param("~port", "/dev/ttyUSB0")#port のdefault値のjoint_s設定
     baud = rospy.get_param("~baud", 115200)
     print(port, baud)
     mc = MyCobot(port, baud)
